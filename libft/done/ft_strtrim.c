c7301b959e6d603ca356e366b801dc6f0eb79d14
jellytusmaximus <36309805+jellytusmaximus@users.noreply.github.com>
2023-09-14 19:57:08 +0800

    From 42 PC

---
+++ libft/done/ft_strtrim.c
@@ -0,0 +1,56 @@
+#include "libft.h"
+
+static int	ft_from_start(char const *s1, char const *set)
+{
+	int	start;
+
+	start = 0;
+	while (start < (int)ft_strlen((char *)s1))
+	{
+		if (!ft_strchr(set, s1[start]))
+			break ;
+		start++;
+	}
+	return (start);
+}
+
+static int	ft_from_end(char const *s1, char const *set)
+{
+	int	end;
+
+	end = ft_strlen((char *)s1) - 1;
+	while (end > 0)
+	{
+		if (!ft_strchr(set, s1[end]))
+			break ;
+		end--;
+	}
+	return (end);
+}
+
+char	*ft_strtrim(char const *s1, char const *set)
+{
+	int		start;
+	int		end;
+	char	*res;
+
+	if (!s1)
+	{
+		res = malloc(1 * sizeof(char));
+		res[0] = '\0';
+		return (res);
+	}
+	end = ft_from_end(s1, set);
+	start = ft_from_start(s1, set);
+	if (start > end)
+	{
+		res = malloc(1 * sizeof(char));
+		res[0] = '\0';
+		return (res);
+	}
+	res = malloc((end - start + 1 + 1) * sizeof(char));
+	if (res == NULL)
+		return (NULL);
+	ft_strlcpy(res, (char *)&s1[start], end - start + 1 + 1);
+	return (res);
+}
