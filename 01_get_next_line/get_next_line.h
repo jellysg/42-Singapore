f68465062739f1003189975ea183224f5b302a7f
JellytusMaximus <jeremygohct@gmail.com>
2023-10-03 00:32:27 +0800

    gnl init

---
+++ 01_get_next_line/get_next_line.h
@@ -0,0 +1,21 @@
+#ifndef GET_NEXT_LINE_H
+# define GET_NEXT_LINE_H
+
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 10 
+
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+
+typedef struct	s_list
+{
+	char	*str_buf;
+	struct	s_list	*next;
+}	t_list;
+
+# endif
+# endif
+/*	first endif is for GET_NEXT_LINE_H
+	second endif is for BUFFER_SIZE
+*/
