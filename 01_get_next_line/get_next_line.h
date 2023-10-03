0dca925b679fec43777a9cfdec52dd1c54f46807
jellytusmaximus <36309805+jellytusmaximus@users.noreply.github.com>
2023-10-03 20:16:51 +0800

    Update get_next_line.h

--- 01_get_next_line/get_next_line.h
+++ 01_get_next_line/get_next_line.h
@@ -2,20 +2,28 @@
 # define GET_NEXT_LINE_H
 
 # ifndef BUFFER_SIZE
-#  define BUFFER_SIZE 10 
+#  define BUFFER_SIZE 10
+# endif
 
-#  include <fcntl.h>
-#  include <stdlib.h>
-#  include <unistd.h>
+# include <fcntl.h>
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdio.h>
 
 typedef struct s_list
 {
-	char			*str_buf;
+	char			*str_buffer;
 	struct s_list	*next;
-}	t_list;
+}				t_list;
 
-# endif
+int		found_newline(t_list *list);
+int		len_to_newline(t_list *list);
+char	*get_next_line(int fd);
+char	*get_line(t_list *list);
+void	copy_string(t_list *list, char *str);
+void	clear_list(t_list **list);
+void	dealloc(t_list **list, t_list *clean_node, char *buffer);
+void	create_list(t_list **list, int fd);
+t_list	*find_last_node(t_list *list);
+
 #endif
-/*	first endif is for GET_NEXT_LINE_H
-	second endif is for BUFFER_SIZE
-*/
