88264af67ebc799f18eab59206a86ff7eec9dd8c
jellytusmaximus <36309805+jellytusmaximus@users.noreply.github.com>
2023-10-03 14:28:22 +0800

    Update get_next_line.h

--- 01_get_next_line/get_next_line.h
+++ 01_get_next_line/get_next_line.h
@@ -4,18 +4,18 @@
 # ifndef BUFFER_SIZE
 #  define BUFFER_SIZE 10 
 
-# include <fcntl.h>
-# include <stdlib.h>
-# include <unistd.h>
+#  include <fcntl.h>
+#  include <stdlib.h>
+#  include <unistd.h>
 
-typedef struct	s_list
+typedef struct s_list
 {
-	char	*str_buf;
-	struct	s_list	*next;
+	char			*str_buf;
+	struct s_list	*next;
 }	t_list;
 
 # endif
-# endif
+#endif
 /*	first endif is for GET_NEXT_LINE_H
 	second endif is for BUFFER_SIZE
 */
