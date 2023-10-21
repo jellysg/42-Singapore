b27f2ed14a3a829790a61bf0b298354ac2ad7a17
jellytusmaximus <36309805+jellytusmaximus@users.noreply.github.com>
2023-10-21 19:45:08 +0800

    final

--- 01_ft_printf/ft_printf.c
+++ 01_ft_printf/ft_printf.c
@@ -1,31 +1,32 @@
 #include "ft_printf.h"
 
-static size_t	conversion(const char *c, size_t *i, va_list *argptr)
+static size_t	checkformat(const char *c, size_t *i, va_list *argptr)
 {
+	char	format;
+
 	*i = *i + 1;
-	if (*(c + 1) == 'c')
+	format = *(c + 1);
+	if (format == 'c')
 		return (ft_putchar((char) va_arg(*argptr, int)));
-	else if (*(c + 1) == 's')
+	else if (format == 's')	
 		return (ft_putstr(va_arg(*argptr, const char *)));
-	else if (*(c + 1) == 'p')
+	else if (format == 'p')
 		return (ft_putaddress(va_arg(*argptr, void *)));
-	else if (*(c + 1) == 'd' || *(c + 1) == 'i')
+	else if (format == 'd' || format == 'i')
 		return (ft_putnbr(va_arg(*argptr, int)));
-	else if (*(c + 1) == 'u')
+	else if (format == 'u')
 		return (ft_putunbr(va_arg(*argptr, unsigned int)));
-	else if (*(c + 1) == 'x')
+	else if (format == 'x')
 		return (ft_puthex_l(va_arg(*argptr, int)));
-	else if (*(c + 1) == 'X')
+	else if (format == 'X')
 		return (ft_puthex_u(va_arg(*argptr, int)));
-	else if (*(c + 1) == '%')
+	else if (format == '%')
 		return (ft_putchar('%'));
-	else if (*(c + 1) && *(c + 2))
-		return (ft_putchar(*(c)) + ft_putchar(*(c + 1)));
+	else if (format && (format + 1))
+		return (ft_putchar(*(c)) + ft_putchar(format));
 	else
-	{
 		*i -= 1;
-		return (-1);
-	}
+	return (-1);
 }
 
 int	ft_printf(const char *str, ...)
@@ -43,7 +44,7 @@ int	ft_printf(const char *str, ...)
 	while (str[i])
 	{
 		if (str[i] == '%')
-			tmp = conversion(str + i, &i, &argptr);
+			tmp = checkformat(str + i, &i, &argptr);
 		else
 			tmp = ft_putchar(str[i]);
 		if (tmp >= 0)
